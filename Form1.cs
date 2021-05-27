b93f524ce848aa48918bd6c5a7a7ad5f9612963a
Abenezer Bekele <79157618+Abenezer1422@users.noreply.github.com>
2021-05-27 12:26:13 -0500

    DM

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DM
+{
+    public partial class Form1 : Form
+    {
+        static int i = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextBox tb = new TextBox();
+
+            Point p = new Point(230 + i, 40 * i);
+            tb.Location = p;
+
+            this.Controls.Add(tb);
+            i++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random random=new Random();
+            
+            
+            List<string> list = new List<string>();
+            foreach (Control c in this.Controls)
+            {
+                if (c is TextBox)
+                    list.Add((c as TextBox).Text);
+            }
+            int index = random.Next(list.Count);
+            label4.Text = list[index];
+
+        }
+
+        
+    }
+}
